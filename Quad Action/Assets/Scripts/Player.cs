5d3650169faf5ee939cb4e115845336a1510308e
강동윤 <davidkang8111@gmail.com>
2022-01-06 20:01:27 +0900

    20220106
    kjca20220106

--- Quad Action/Assets/Scripts/Player.cs
+++ Quad Action/Assets/Scripts/Player.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
     float hAxis;
     float vAxis;
+    bool wDown;
 
     Vector3 moveVec;
+
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
 
     void Start()
     {
@@ -16,9 +25,17 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        hAxis = Input.GetAxisRaw("Horizental");
+        hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
 
-        moveVec = new Vector3(hAxis, 0, vAxis);
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * (wDown ? 0.3f : 1.0f) * Time.deltaTime;
+
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+        anim.SetBool("isWalk", wDown);
+
+        transform.LookAt(transform.position + moveVec);
     }
 }
